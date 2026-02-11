c95fea3c1174af47961e1a9e0f09ade337107524
Tristan Germer <tgermer@me.com>
2026-02-11 13:08:45 +0100

    Adopt IBM Plex Sans and refine UI typography

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import "@fontsource/ibm-plex-sans/latin-400.css";
+import "@fontsource/ibm-plex-sans/latin-500.css";
+import "@fontsource/ibm-plex-sans/latin-600.css";
+import "@fontsource/ibm-plex-sans/latin-700.css";
 import App from "./App";
 import "./index.css";
 
